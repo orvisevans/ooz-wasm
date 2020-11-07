8281243a2864b2ad3a24b19a9254f5a79784aa79
orvis evans <b.orvis@gmail.com>
2020-11-06 18:28:22 -0800

    adding node flag to use wasm-simd functionality.

--- index.js
+++ index.js
@@ -1,26 +1,33 @@
-'use strict'
+"use strict";
 
-var oozModule = require('./build/ooz.js');
+var oozModule = require("./build/ooz.js");
 
 var OOZ_SAFE_SPACE = 64;
 
 var module_ = null;
 var lastDecompressionPtr_ = 0;
 
-function load () {
+function load() {
+  addNodeFlag();
   if (!module_) {
     module_ = oozModule();
   }
   return module_;
 }
 
+function addNodeFlag() {
+  const argv = Array.from(process.argv);
+  const flag = "--experimental-wasm-simd";
+  if (!argv.includes(flag)) process.argv.push(flag);
+}
+
 /**
- * @param {Uint8Array} data 
- * @param {number} rawSize 
+ * @param {Uint8Array} data
+ * @param {number} rawSize
  * @returns {Promise<Uint8Array>}
  */
-function decompressUnsafe (data, rawSize) {
-  return load().then(module => {
+function decompressUnsafe(data, rawSize) {
+  return load().then((module) => {
     if (lastDecompressionPtr_) {
       module._free(lastDecompressionPtr_);
       lastDecompressionPtr_ = 0;
@@ -33,30 +40,32 @@ function decompressUnsafe (data, rawSize) {
     lastDecompressionPtr_ = decompressedPtr;
 
     var res = module._Kraken_Decompress(
-      compressedPtr, data.byteLength,
-      decompressedPtr, rawSize
+      compressedPtr,
+      data.byteLength,
+      decompressedPtr,
+      rawSize
     );
 
     module._free(compressedPtr);
 
     if (res < 0) {
-      throw new Error('Failed to decode');
+      throw new Error("Failed to decode");
     }
     if (res !== rawSize) {
-      throw new Error('Decompresed size is different from expected');
+      throw new Error("Decompresed size is different from expected");
     }
 
     return module.HEAPU8.subarray(decompressedPtr, decompressedPtr + rawSize);
-  })
+  });
 }
 
 /**
- * @param {Uint8Array} data 
- * @param {number} rawSize 
+ * @param {Uint8Array} data
+ * @param {number} rawSize
  * @returns {Promise<Uint8Array>}
  */
-function decompress (data, rawSize) {
-  return decompressUnsafe(data, rawSize).then(decompressed => {
+function decompress(data, rawSize) {
+  return decompressUnsafe(data, rawSize).then((decompressed) => {
     const decompressedCopy = new Uint8Array(decompressed.byteLength);
     decompressedCopy.set(decompressed);
 
